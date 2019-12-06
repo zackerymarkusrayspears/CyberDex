e4a69df3c17c4abe49556c9feddfe01854afb2a9
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-12-06 15:22:59 -0600

    Added DataSchema.js to backend folder, modified server.js to give data to DataSchema Values when performing request, and edited secret to be ignored.

---
+++ backend/DataSchema.js
@@ -0,0 +1,19 @@
+// Creating a Mongoose Schema that we will use to make sure the data we send to our MongoDB is formatted correctly.
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DataSchema = new Schema(
+    {
+        id: Number,
+        number: Number,
+        buiding: Number,
+        extensions: Number,
+        firstName: String,
+        lastName: String,
+        room: Number
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Data', DataSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,51 @@
-//Mongoose - allows us to set up schemas for the data we store in our MongoDB database. We will also set up our database connection using Mongoose.
+// Mongoose - allows us to set up schemas for the data we store in our MongoDB database. We will also set up our database connection using Mongoose.
 const mongoose = require('mongoose');
-//Express - framework for NodeJS. Handles our routing and  connection info (port, etc.). 
+// Express - framework for NodeJS. Handles our routing and  connection info (port, etc.). 
 const express = require('express');
-//Morgan - Handles logging stuff for databases.
+// Morgan - Handles logging stuff for databases.
 const logger = require('morgan');
-//Body-Parser - Makes sure the body of our requests is formatted correctly (in this case, we'll use JSON).
+// Body-Parser - Makes sure the body of our requests is formatted correctly (in this case, we'll use JSON).
 const bodyParser = require('body-parser');
 
-//Import the getSecret function from the secret.js file
+// Import the getSecret function from the secret.js file
 const getSecret = require('./secret');
 
-//Import the Mongoose Schema for our data
+// Import the Mongoose Schema for our data
 const Data = require('./DataSchema');
 
-//Constant to hold the port that we are going to use to connect
+// Constant to hold the port that we are going to use to connect
 const API_PORT = 3001;
 
-//Create an Express app that will run on our Node server and route our requests
+// Create an Express app that will run on our Node server and route our requests
 const app = express();
 
-//Create an Express router which actually handles the routing
+// Create an Express router which actually handles the routing
 const router = express.Router();
 
-//Use Mongoose to set up a connection to the database
+// Use Mongoose to set up a connection to the database
 mongoose.connect(getSecret('dbUrl'), { useNewUrlParser: true, useFindAndModify: false });
 
-//Reference to our database connection
+// Reference to our database connection
 let db = mongoose.connection;
 
-//Use the database connection to print out an error if one occurs when we try to connect to the database
+// Use the database connection to print out an error if one occurs when we try to connect to the database
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-//Configure body-parser and morgan
+// Configure body-parser and morgan
 app.use(bodyParser.urlencoded({ extended: false })); //Used for parsing data formatted in the x-www-form-urlencoded format
 app.use(bodyParser.json()); //Used for parsing data formatted in the JSON format
 app.use(logger('dev'));
 
-//Default route that will run whenever we first connect to the server
+// Default route that will run whenever we first connect to the server
 router.get('/', (req, res) => {
     res.json({ message: 'HELLO WORLD' });
 });
 
-//Route to retrieve data from the database
+// Route to retrieve data from the database
 router.get('/getData', (req, res) => {
-    //Use Mongoose to find data with the given schema
+    // Use Mongoose to find data with the given schema
     Data.find((err, data) => {
+
         console.log(data);
 
         if (err) {
@@ -55,27 +56,32 @@ router.get('/getData', (req, res) => {
     });
 });
 
-//Route that will put new information in the database
+// Route that will put new information in the database
 router.post('/postData', (req, res) => {
-    //A new instance of the Mongoose Schema which we will send to the database
+    // A new instance of the Mongoose Schema which we will send to the database.
     let newData = new Data();
 
-    //Pull the ID and message from the body of the request
-    const { id, message } = req.body;
+    // Pull the ID and message from the body of the request.
+    const { id, number, building, extensions, firstName, lastName, room } = req.body;
 
-    //If ID does not have a value and is not equal to 0 or message doesn't have a value, return an error
-    if ((!id && id !== 0) || !message) {
+    // Configure the schema object.
+    newData.id = id;
+    newData.number = number;
+    newData.building = building;
+    newData.extensions = extensions;
+    newData.firstName = firstName;
+    newData.lastName = lastName;
+    newData.room = room;
+
+    // If ID does not have a value and is not equal to 0 or the firstName and lastName doesn't have a value, return an error.
+    if ((!id && id !== 0) || (firstName || lastName === false)) {
         return res.json({
             success: false,
             error: 'INVALID INPUT'
         })
     }
 
-    //Configure the schema object
-    newData.id = id;
-    newData.message = message;
-
-    //Try to save to the database
+    // To save to the database.
     newData.save(err => {
         if (err) {
            return res.json({ success: false, error: err }); 
@@ -85,7 +91,7 @@ router.post('/postData', (req, res) => {
     });
 });
 
-//Route to delete a given object from our database
+// Route to delete a given object from our database.
 router.delete('/deleteData', (req, res) => {
     Data.deleteOne({ id: req.body.id }, err => {
         if (err) {
@@ -96,7 +102,7 @@ router.delete('/deleteData', (req, res) => {
     });
 });
 
-//Route to update an existing object in our database
+// Route to update an existing object in our database.
 router.post('/updateData', (req, res) => {
     Data.updateOne({ id: req.body.id }, req.body.update, err => {
         if (err) {
@@ -107,8 +113,8 @@ router.post('/updateData', (req, res) => {
     });
 });
 
-//Tell Express to use a certain path and to use the router we set up
+// Tell Express to use a certain path and to use the router we set up.
 app.use('/api', router);
 
-//Tell Express to listen for requests on the appropriate port
+// Tell Express to listen for requests on the appropriate port.
 app.listen(API_PORT, () => console.log('LISTENING ON PORT: ' + API_PORT));
