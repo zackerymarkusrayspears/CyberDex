6abb0f117c94647b02a98f47dda2819789ac6cb7
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-12-06 20:29:16 -0600

    Added content to h1 to Search and Index Components to test Router.

---
+++ client/src/Components/Index/Index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Index.css';
+
+export default class Index extends Component {
+    render() {
+        return(
+            <h1>Index</h1>
+        );
+    }
+}
