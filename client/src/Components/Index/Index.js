1c0065dd062dd951cbe1c7ba8ce50a526842649f
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-12-07 02:43:36 -0600

    Installed axios to app, Added axios GET calls for sheet titles.

--- client/src/Components/Index/Index.js
+++ client/src/Components/Index/Index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Index.css';
 
 export default class Index extends Component {
@@ -7,7 +8,50 @@ export default class Index extends Component {
         this.state = {
             addId: '',
             addArray: [],
+            titleArray: [],
+            dbArray: [],
         }
+    }
+
+    getData = () => {
+
+        // Access current arrays.
+        const { addArray } = this.state;
+        // Bind this as self.
+        const self = this;
+        // Temporary acces token.
+        const accessToken = 'ya29.Il-0B9OqFcWXBX_ny1ijtOLa7q4LcWRD6Mv9ryNWPp5igFsGryiSlyXk_v9PYt1LLZXMpGNmS43IWVAWoa6tXzkm68RJAPmpLD6lnSLvi_9ZqY-9RfgwT_s2bsyuF-R3YQ';
+
+        // For each SpreadsheetID in the addArray.
+        addArray.forEach( id => {
+            // Perform a GET on the ID.
+            axios({
+                method: 'GET',
+                url: `https://sheets.googleapis.com/v4/spreadsheets/${id}`,
+                headers: { Authorization: `Bearer ${accessToken}` }
+        
+            }).then((response) => {
+                console.log(response);
+                // response.data.sheet of sheets.properties.title
+                var newArray = [];
+                var dbArray = [id];
+
+                response.data.sheets.map(event => {
+                    return newArray = newArray.concat(event.properties.title);
+                });
+
+                dbArray = dbArray.concat(newArray);
+                
+                self.setState({ 
+                    titleArray: newArray,
+                    dbArray: dbArray
+                });
+
+            }).catch((error) => {
+              console.log(error);
+            });
+
+        });
     }
 
     addToArray = spreadsheetId => {
@@ -32,18 +76,19 @@ export default class Index extends Component {
         // Splice from the index of the mapped key.
         newArray.splice(i, 1);
         // Set New array as the state.
-        this.setState({ addArray: newArray});
+        return this.setState({ addArray: newArray});
     }
 
     displayArray = () => {
         // Access current array.
-        const { addArray } = this.state;
+        const { addArray, titleArray } = this.state;
         // Iterate through array.
         return addArray.map((event, i) => (
             <li key={i}>
                 {event}
                 {/* Button to remove listed item from Array */}
-                <button onClick={() => this.removeFromArray(i)}>X</button>
+                <button onClick={() => this.removeFromArray()}>X</button>
+                {titleArray.length !== 0 ? <small>{'Sheets: ' + titleArray}</small> : null}
             </li>
         ));
     }
@@ -70,6 +115,9 @@ export default class Index extends Component {
                 <ol>
                     {this.displayArray()}
                 </ol>
+                <button
+                    onClick={() => this.getData()}
+                >Submit</button>
             </div>
         );
     }
