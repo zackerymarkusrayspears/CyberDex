d7532f4b0d05e26c03dc387566652f85e4216c56
CodyThomasCrump <codytcrump@gmail.com>
2019-12-08 10:43:27 -0600

    Commiting all the json files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
-import TeacherSearcher from './TeacherSearcher'
 import './App.css';
+import './Components/Search/Search'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component{
   render() {
     return(
-      <div>
-        <TeacherSearcher />
-      </div>
+      <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/'></Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* The Switch component looks through the Route components inside of it and renders the first one that matches the current URL */}
+      <Switch>
+        <Route exact path='/'>
+          <Search />                                                                        
+        </Route>
+        <Route path='/SearchForm'>
+          <SearchForm />
+        </Route>
+      </Switch>
+    </Router>
     );
   }
 }
--- client/src/addToDatabase.js
+++ client/src/addToDatabase.js
@@ -1,1 +1,13 @@
-import
+import React, { Component } from 'react';
+
+class AddToDatabase extends Component {
+    render() {
+        return(
+            <div>
+                <p>Yo mama</p>
+            </div>
+        )
+    }
+}
+
+export default AddToDatabase;
