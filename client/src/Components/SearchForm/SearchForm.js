9417e82e2fe5e7c7f2371d5088affe2177969e8b
Jacob Richardson <jacobrrichardson41@gmail.com>
2019-12-10 14:20:05 -0600

    Added the Select Button and added each school in a select button

--- client/src/Components/SearchForm/SearchForm.js
+++ client/src/Components/SearchForm/SearchForm.js
@@ -8,6 +8,52 @@ export default class SearchForm extends Component {
 
         return(
             <form className='SearchForm'>
+                <label>District:
+                    <select>
+                        <option 
+                            selected
+                            value={''}
+                        >Select</option>
+                        <option 
+                            value={'barrenCounty'}
+                        >Barren County</option>
+                    </select>
+                </label>
+                <label>School:
+                    <select>
+                        <option 
+                            selected
+                            value={''}
+                        >Select</option>
+                        <option 
+                            value={'redCross'}
+                        >Red Cross Elementary </option>
+                        <option 
+                            value={'templeHill'}
+                        >Temple Hill Elementary </option>
+                          <option 
+                            value={'parkCity'}
+                        >Park City Elementary </option>
+                          <option 
+                            value={'northJackson'}
+                        >North Jackson Elementary</option>
+                          <option 
+                            value={'hiseville'}
+                        >Hiseville Elementary</option>
+                          <option 
+                            value={'austinTracy'}
+                        >Austin Tracy Elementary</option>
+                          <option 
+                            value={'barrenMiddle'}
+                        >Barren County Middle School</option>
+                          <option 
+                            value={'trojanAcademy'}
+                        >Trojan Academy</option>
+                          <option 
+                            value={'barrenHigh'}
+                        >Barren County High School</option>
+                    </select>
+                </label>
                 <input  
                     className='SearchForm-input' 
                     type='text' 
