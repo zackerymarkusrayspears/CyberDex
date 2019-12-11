b34bea3cb77ab7ace3995e8d1295f008067c5ff1
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-12-11 14:35:00 -0600

    Added map to display current dbArray.

--- client/src/Components/DeleteIndex/DeleteIndex.js
+++ client/src/Components/DeleteIndex/DeleteIndex.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react';
 import './DeleteIndex.css';
 
 export default class DeleteIndex extends Component {
+
+    displayDB() {
+
+
+    }
+
     render() {
+
+        const { dbData } = this.props;
+
         return(
-            <h1>DeleteIndex</h1>
+            <div className='DeleteIndex'>
+                <h3>Select Spreadsheet(s) to Remove:</h3>
+                <ol>
+                    {dbData.map((data, i) => {
+                        return (
+                            <li key={i}>
+                                {/* Button to add item listed to deleteArray */}
+                                <button onClick={() => {
+                                    this.addToArray(i)
+                                }}>X</button>
+                                {/* Display data from each event */}
+                                <h6>{data.spreadsheetTitle}</h6>
+                                <small>{'SpreadsheetID: ' + data.spreadsheetId}</small>
+                                <br />
+                                <small>{'Sheets: ' + data.sheetTitle}</small>
+                            </li>
+                        );
+                    })}
+                </ol>
+            </div>
+
         );
     }
 }
