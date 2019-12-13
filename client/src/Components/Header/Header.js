21fbcbf42f5089a2c567dd64010775f52b3966ad
CodyThomasCrump <codytcrump@gmail.com>
2019-12-13 10:03:29 -0600

    Added styling to the apps components

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './Header.css';
 
-export default class Header extends Component {
-
+class Header extends Component {
     render() {
         const { children } = this.props;
 
         return(
             <div className='Header'>
                 <div className='Header-main'>
+                    <img className='BC-logo' src="https://www.barren.k12.ky.us/docs/district/brand%20images/we%20are%20bc%201%20burgundy%20filled%20white%20font.jpg?id=83313" alt="Barren County Logo"></img>
                     {children}
                 </div>
             </div>
         );
     }
 }
+
+export default Header;
