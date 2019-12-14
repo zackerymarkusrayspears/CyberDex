1b2bfae0dc4c5c7a0173e16c6073d1fda82bb107
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-12-13 21:12:48 -0600

    Added content to display for Card Component.

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -2,12 +2,50 @@ import React, { Component } from 'react';
 import './Card.css';
 
 class Card extends Component {
+
     render() {
-        return(
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        )
+
+        const { phoneTag, name, room, extension, phoneNumber, note, capitalize } = this.props;
+        return <div className='Card'>
+            <li className='Card'>
+                {name !== null ? (
+                    <h3 className='Card-name'>{capitalize(name)}</h3>
+                ) : (
+                    ''
+                )}
+                <div className='Card-body'>
+                    <div className='Card-bodyLeft'>
+                        {phoneTag !== null ? (
+                            <h6 className='Card-phoneTag'>{`Phone Tag: ${capitalize(phoneTag)}`}</h6>
+                        ) : (
+                            ''
+                        )}
+                        {room !== null ? (
+                            <h6 className='Card-room'>{`Room: ${capitalize(room)}`}</h6>
+                        ) : (
+                            ''
+                        )}
+                        {extension !== null ? (
+                            <h6 className='Card-extension'>{`Extension: ${extension}`}</h6>
+                        ) : (
+                            ''
+                        )}
+                        {phoneNumber !== null ? (
+                            <h6 className='Card-phoneNumber'>{`Phone Number: ${phoneNumber}`}</h6>
+                        ) : (
+                            ''
+                        )}
+                    </div>
+                    <div className='bodyRight'>
+                        {note !== null ? (
+                            <p className='Card-note'>{`Note: ${capitalize(note)}`}</p>
+                        ) : (
+                            ''
+                        )}
+                    </div>
+                </div>
+            </li>
+        </div>
     }
 }
 
