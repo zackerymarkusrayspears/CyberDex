2e136ad9e385ebff2682ab6c6bf1b681d5a1b249
CodyThomasCrump <codytcrump@gmail.com>
2019-12-16 14:50:52 -0600

    Added styling to the header component and the home and index buttons

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -8,9 +8,12 @@ class Header extends Component {
         return(
             <div className='Header'>
                 <div className='Header-main'>
-                    <img className='BC-logo' src="https://www.barren.k12.ky.us/docs/district/brand%20images/we%20are%20bc%201%20burgundy%20filled%20white%20font.jpg?id=83313" alt="Barren County Logo"></img>
+                    <img className='BC-logo' src="https://www.barren.k12.ky.us/docs/district/brand%20images/district%20logo/we%20are%20bc%201%20burgundy%20filled%20white%20font.png?id=85394" alt="Barren County Logo"></img>
                     {children}
                 </div>
+                    {/* <div className='Sign-in'>
+                        <p>Sign In</p>
+                    </div> */}
             </div>
         );
     }
